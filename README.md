862f0176db02d896b899f7e76d6f9350020a8fbd
Priyaraj7 <suppichala@gmail.com>
2020-02-17 16:53:32 -0800

    add README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# **Movie Tickets**
+
+#### Arrays and Looping Javascript
+
+#### By _**Supriya**_
+
+## Description:
+
+In this webpage allow the user to select the name of a movie, the time of day and age. After submitting the form it shows how much movie ticket will cost, based on the factors likes movie, time and age. Senior, child, morning and afternoon ticket price is less than the regular ticket price.
+
+You can check out the page [here](https://priyaraj7.github.io/Movie-ticket/).
+
+## Setup/Installation Requirements
+
+- Open the Terminal
+- Clone the repository
+- Navigate to the directory
+- Open `index.html`
+
+## Technologies Used
+
+- HTML
+- Bootstrap
+- JQuery
+- JavaScript
+
+Copyright (c) 2020 (Supriya)
--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,82 @@
 // construction- class
 function MovieTicket(morning, afternoon, evening) {
-this.morning = morning;
-this.afternoon = afternoon;
-this.evening = evening;
+  this.morning = morning;
+  this.afternoon = afternoon;
+  this.evening = evening;
 }
 //
-MovieTicket.prototype.getMorningPrice = function(){
-    return this.morning;
-}
-MovieTicket.prototype.getAfternoonPrice = function(){
-    return this.afternoon;
-}
-MovieTicket.prototype.getEveningPrice = function(){
-    return this.evening;
-}
+MovieTicket.prototype.getMorningPrice = function() {
+  return this.morning;
+};
+MovieTicket.prototype.getAfternoonPrice = function() {
+  return this.afternoon;
+};
+MovieTicket.prototype.getEveningPrice = function() {
+  return this.evening;
+};
 
 //
 
 function BoxOffice() {
-    this.movies = {};
+  this.movies = {};
 }
 
-BoxOffice.prototype.addMovie = function(name, age, morningPrice, eveningPrice, afterNoonPrice) {
-    var movie = this.movies[name] || {};
-    movie[age] = new MovieTicket(morningPrice, afterNoonPrice, eveningPrice);
-    this.movies[name] = movie;
-}
-
+BoxOffice.prototype.addMovie = function(
+  name,
+  age,
+  morningPrice,
+  eveningPrice,
+  afterNoonPrice
+) {
+  var movie = this.movies[name] || {};
+  movie[age] = new MovieTicket(morningPrice, afterNoonPrice, eveningPrice);
+  this.movies[name] = movie;
+};
 
 BoxOffice.prototype.getPrice = function(name, age, time) {
-    var movie = this.movies[name];
-    if(!movie) {
-        throw new Error('Movie ' + name + ' not found');
-    }
-    var age = movie[age];
-    if(!age) {
-        throw new Error('Age group' + age + ' not found');
-    }
-    switch(time) {
-        case 'morning':
-            return age.getMorningPrice();
-        case 'afternoon':
-            return age.getAfternoonPrice();
-        case 'evening': 
-            return age.getEveningPrice();
-        default:
-            throw new Error('Time ' + time + 'not found');
-    }
-}
+  var movie = this.movies[name];
+  if (!movie) {
+    throw new Error("Movie " + name + " not found");
+  }
+  var age = movie[age];
+  if (!age) {
+    throw new Error("Age group" + age + " not found");
+  }
+  switch (time) {
+    case "morning":
+      return age.getMorningPrice();
+    case "afternoon":
+      return age.getAfternoonPrice();
+    case "evening":
+      return age.getEveningPrice();
+    default:
+      throw new Error("Time " + time + " not found");
+  }
+};
 
+// User Interface
+var boxOffice = new BoxOffice();
 
+boxOffice.addMovie("frozen", "adult", "$22", "$22", "$25");
+boxOffice.addMovie("frozen", "senior", "$20", "$20", "$23");
+boxOffice.addMovie("frozen", "child", "$10", "$10", "$15");
 
+boxOffice.addMovie("jumanji", "adult", "$15", "$15", "$20");
+boxOffice.addMovie("jumanji", "senior", "$15", "$15", "$20");
+boxOffice.addMovie("jumanji", "child", "$5", "$5", "$9");
 
+boxOffice.addMovie("parasite", "adult", "$25", "$25", "$30");
+boxOffice.addMovie("parasite", "senior", "$25", "$25", "$30");
+boxOffice.addMovie("parasite", "child", "$10", "$10", "$15");
+
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+    var movie = $("#movies option:selected").val();
+    var time = $("#time option:selected").val();
+    var age = $("#age option:selected").val();
+
+    $("#result").append(boxOffice.getPrice(movie, age, time));
+  });
+});
